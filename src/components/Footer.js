fd744c657f2e1da0802063292953f251e492dfe1
RupertHartmann <rupert.hartmann@archetype.co>
2024-11-10 11:13:55 +0000

    Resolve merge conflicts

---
+++ src/components/Footer.js
@@ -0,0 +1,44 @@
+import React from "react"
+import small_logo from "../images/Logo .svg"
+
+const Footer = () => {
+    return(
+        <footer className="">
+            <section>
+            <div className="company-info">
+                <img src={small_logo} alt=""/>
+                <p>We believe in creating exceptional dining experiences that bring people together. Our menu combines locally sourced ingredients with bold, international flavors, offering a variety of dishes to satisfy every taste. Whether you're enjoying a casual lunch or celebrating a special occasion, our welcoming atmosphere and attentive service make every meal memorable. Join us for a delicious journey that highlights the best of fresh, seasonal ingredients, thoughtfully prepared and served with care.</p>
+            </div>
+            <div>
+                <h3>Important Links</h3>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">About</a></li>
+                <li><a href="/">Menu</a></li>
+                <li><a href="/">Reservations</a></li>
+                <li><a href="/">Order Online</a></li>
+                <li><a href="/">Login</a></li>
+            </ul>
+            </div>
+            <div>
+                <h3>Contact</h3>
+            <ul>
+            <li>Address: <br/> 564 Oxford Street, London</li>
+                <li>Phone: <br/> +00 145 676 009</li>
+                <li>Email: <br/> little@lemon.co.uk</li>
+            </ul>
+            </div>
+            <div>
+                <h3>Social Media Links</h3>
+            <ul>
+                <li><a href="/">Facebook</a></li>
+                <li><a href="/">Instagram</a></li>
+                <li><a href="/">Twitter</a></li>
+            </ul>
+            </div>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer;
