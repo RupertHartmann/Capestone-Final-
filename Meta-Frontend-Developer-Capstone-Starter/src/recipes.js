74df672c7e4b298647f0612089cdacb2f521b949
RupertHartmann <rupert.hartmann@archetype.co>
2024-11-11 19:10:56 +0000

    Final Project

---
+++ Meta-Frontend-Developer-Capstone-Starter/src/recipes.js
@@ -0,0 +1,31 @@
+const recipes = [
+    { 
+      id: 1,
+      title: "Greek salad",
+      price: 8.99,
+      image: "https://i.ibb.co/68c9bLv/greek-salad.jpg",
+      description:
+        "A classic Greek salad made with crisp cucumbers, ripe tomatoes, red onions, Kalamata olives, and creamy feta cheese, all drizzled with rich olive oil and sprinkled with oregano.",
+    },
+    {
+      id: 2,
+      title: "Bruchetta",
+      price: 7.99,
+      image: "https://i.ibb.co/317GqZv/Bruchetta.png",
+      description:
+        "An Italian appetizer consisting of toasted bread topped with a mixture of fresh ingredients, typically including diced tomatoes, garlic, basil, olive oil, and salt. It's a simple, flavorful dish often served as a starter or snack.",
+    },
+    {
+      id: 3,
+      title: "Lemon Cake",
+      price: 5.99,
+      image: "https://i.ibb.co/sm6MfcH/desert.jpg",
+      description:
+        "Our Lemon cake is a light, moist dessert infused with fresh lemon juice and zest, offering a sweet and tangy flavor. Topped with a lemon glaze or frosting, it's a refreshing and flavorful treat perfect for any occasion.",
+    },
+  
+  ];
+  
+  export default recipes;
+  
+  
